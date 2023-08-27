ca681aba90d0ae94512db1fde4a6a70e5b763c51
Md Monirul Islam <47271818+dev-monirul@users.noreply.github.com>
2023-08-27 06:27:00 +0600

    new course add completed
    new course add completed

--- admin/routes/web.php
+++ admin/routes/web.php
@@ -4,6 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\VisitorController;
 use App\Http\Controllers\ServiceController;
+use App\Http\Controllers\CoursesController;
 
 /*
 |--------------------------------------------------------------------------
@@ -30,3 +31,10 @@ Route::post('/ServiceUpdate',[ServiceController::class, 'ServiceUpdate']);
 Route::post('/ServiceAdd',[ServiceController::class, 'ServiceAdd']);
 
 
+// Admin panel courses management
+Route::get('/courses',[CoursesController::class, 'CoursesIndex']);
+Route::get('/getCoursesData',[CoursesController::class, 'getCoursesData']);
+Route::post('/CoursesDelete',[CoursesController::class, 'CoursesDelete']);
+Route::post('/CoursesDetails',[CoursesController::class, 'getCoursesDetails']);
+Route::post('/CoursesUpdate',[CoursesController::class, 'CoursesUpdate']);
+Route::post('/CoursesAdd',[CoursesController::class, 'CoursesAdd']);
